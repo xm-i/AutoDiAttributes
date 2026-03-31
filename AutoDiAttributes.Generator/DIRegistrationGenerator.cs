d098feeb8639bfc1eb45fcccaec74539e21765e6
testakina <testakina@gmail.com>
2026-03-31 20:01:53 +0900

    using整理

--- AutoDiAttributes.Generator/DIRegistrationGenerator.cs
+++ AutoDiAttributes.Generator/DIRegistrationGenerator.cs
@@ -1,8 +1,8 @@
+using System.Collections.Immutable;
+using System.Text;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-
-using System.Text;
-using System.Collections.Immutable;
 
 namespace AutoDiAttributes.Generator;
 
